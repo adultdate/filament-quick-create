2e5df712f5670bf995272aec7c6ec98be585d94a
awcodes <3596800+awcodes@users.noreply.github.com>
2025-06-23 14:35:33 +0000

    Chore: Fix PHP code styling

--- src/Components/QuickCreateMenu.php
+++ src/Components/QuickCreateMenu.php
@@ -93,7 +93,7 @@ class QuickCreateMenu extends Component implements HasActions, HasForms
 
                         if ($page) {
                             $reflectionMethod = new \ReflectionMethod($page, 'getHeaderActions');
-                            $actions = $reflectionMethod->invoke(new $page());
+                            $actions = $reflectionMethod->invoke(new $page);
                             $createAction = collect($actions)->filter(function ($action) {
                                 return $action instanceof CreateAction;
                             })->first();
@@ -124,7 +124,7 @@ class QuickCreateMenu extends Component implements HasActions, HasForms
                             if ($translatableContentDriver = $livewire->makeFilamentTranslatableContentDriver()) {
                                 $record = $translatableContentDriver->makeRecord($model, $data);
                             } else {
-                                $record = new $model();
+                                $record = new $model;
                                 $record->fill($data);
                             }
 
